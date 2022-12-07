ccc4c7e8d72f17dfaa0fa00c3e06d06106dab93b
Ben Elliot <benjiboi214@gmail.com>
2022-12-07 16:10:22 +1100

    Implemented example auth strategy with jwt

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, UseGuards, Req } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { Request } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Req() req: Request) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
+    return req.user;
+  }
+}
