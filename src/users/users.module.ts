11ca952eae6af0e275aa41bb3727269cda32e51d
Ben Elliot <benjiboi214@gmail.com>
2022-12-08 11:54:24 +1100

    Add users service using prisma

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/database/prisma.module';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
+  imports: [PrismaModule],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
 
 // This should be a real class/interface representing a user entity
 export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(private usersRepository: UsersRepository) {}
+
   private readonly users = [
     {
       userId: 1,
@@ -20,5 +25,17 @@ export class UsersService {
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
+  }
+
+  async createOne(params: { email: string; password: string }) {
+    const { email, password } = params;
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+    const userCreateInput: Prisma.UserCreateInput = {
+      email: email,
+      passwordHash: hash,
+      passwordSalt: salt,
+    };
+    this.usersRepository.createUser({ data: userCreateInput });
   }
 }
