df736e008a8f775b2fb285fc3e7a267fd323f2e1
Ben Elliot <benjiboi214@gmail.com>
2022-12-08 11:54:35 +1100

    Add test register endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Post, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Req, Body } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Request } from 'express';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -18,5 +22,10 @@ export class AppController {
   @Get('profile')
   getProfile(@Req() req: Request) {
     return req.user;
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: { email: string; password: string }) {
+    return this.usersService.createOne(createUserDto);
   }
 }
