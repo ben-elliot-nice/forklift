4f7a28687ad05202edbea0d92a1060409775c40d
Ben Elliot <benjiboi214@gmail.com>
2022-12-10 14:23:00 +1100

    Implement find one and createone

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,3 +1,4 @@
+import { Result } from '@badrap/result';
 import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
@@ -6,8 +7,29 @@ import { PrismaService } from 'src/database/prisma.service';
 export class UsersRepository {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(params: { data: Prisma.UserCreateInput }): Promise<User> {
+  async createUser(params: {
+    data: Prisma.UserCreateInput;
+  }): Promise<Result<User, Error>> {
     const { data } = params;
-    return this.prisma.user.create({ data });
+    try {
+      const user = await this.prisma.user.create({ data });
+      return Result.ok(user);
+    } catch (error) {
+      return Result.err(error);
+    }
+  }
+
+  async findOneUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+  }): Promise<Result<User, Error>> {
+    const { where } = params;
+    try {
+      const user = await this.prisma.user.findUniqueOrThrow({
+        where,
+      });
+      return Result.ok(user);
+    } catch (e) {
+      return Result.err(e);
+    }
   }
 }
