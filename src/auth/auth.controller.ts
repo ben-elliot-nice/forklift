ae143f54feed1ac9295c834bd2402a296404a444
Ben Elliot <benjiboi214@gmail.com>
2022-12-10 14:21:03 +1100

    Move auth routes to auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UserExistsError } from 'src/users/errors/user-exists.error';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req: Request) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    const result = await this.usersService.createOne(createUserDto);
+    if (result.isOk) {
+      return result.value;
+    } else {
+      const e = result.error;
+      if (e instanceof UserExistsError) {
+        throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
+      }
+    }
+  }
+}
