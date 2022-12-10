718f50f1d17db10f997707a68d7e444e325942ae
Ben Elliot <benjiboi214@gmail.com>
2022-12-10 14:20:06 +1100

    Move controllers to their own files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,4 @@
-import { Controller, Get, Post, UseGuards, Req, Body } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { Request } from 'express';
-import { UsersService } from './users/users.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(
-    private authService: AuthService,
-    private usersService: UsersService,
-  ) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Req() req: Request) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Req() req: Request) {
-    return req.user;
-  }
-
-  @Post('register')
-  async register(@Body() createUserDto: { email: string; password: string }) {
-    return this.usersService.createOne(createUserDto);
-  }
-}
+export class AppController {}
