5bf5ee10ccdec1333ba602a0e84b6f9a8c9fe745
Ben Elliot <benjiboi214@gmail.com>
2022-12-15 17:00:12 +1100

    Clean up old imports and add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
 import messageQueueConfig from './config/message-queue.config';
 import { UsersModule } from './users/users.module';
 
@@ -10,12 +9,12 @@ import { UsersModule } from './users/users.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [messageQueueConfig],
+      load: [appConfig, messageQueueConfig],
     }),
     AuthModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
